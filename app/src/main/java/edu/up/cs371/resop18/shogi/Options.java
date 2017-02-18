0ff7374d6271e9646d5a6cedd5a29db5b4991473
Resop18 <resop18@up.edu>
2017-02-17 16:31:20 -0800

    More stuff!

--- app/src/main/java/edu/up/cs371/resop18/shogi/Options.java
+++ app/src/main/java/edu/up/cs371/resop18/shogi/Options.java
@@ -1,9 +1,18 @@
 package edu.up.cs371.resop18.shogi;
 
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.Spinner;
 
 /**
  * Created by Javier on 14-Feb-17.
@@ -11,10 +20,15 @@ import android.view.WindowManager;
 
 public class Options extends AppCompatActivity {
 
+    private SeekBar volumeSeekbar = null;
+    private AudioManager audioManager = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.options);
+
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
 
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
@@ -23,7 +37,69 @@ public class Options extends AppCompatActivity {
         int height = dm.heightPixels;
         int width = dm.widthPixels;
 
-        getWindow().setLayout((int)(width * .98), (int)(height * .98));
+        getWindow().setLayout((int)(width * .85), (int)(height * .85));
 
+        final Spinner sounds = (Spinner)findViewById(R.id.Easter);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner_stuff, android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sounds.setAdapter(adapter);
+
+        initControls();
+
+        final MediaPlayer timallen = MediaPlayer.create(this, R.raw.allenarrogh);
+        final MediaPlayer carlin = MediaPlayer.create(this, R.raw.carlinlogic);
+        final MediaPlayer ironman = MediaPlayer.create(this, R.raw.ironman);
+        Button playMe = (Button)findViewById(R.id.EasterEgg);
+        playMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(sounds.getSelectedItem().toString().contentEquals("swoosh")) {timallen.start();}
+                if(sounds.getSelectedItem().toString().contentEquals("bazzinga")){carlin.start();}
+                if(sounds.getSelectedItem().toString().contentEquals("waffles")){ironman.start();}
+            }
+        });
     }
+
+
+
+
+    private void initControls()
+    {
+        try
+        {
+            volumeSeekbar = (SeekBar)findViewById(R.id.vol);
+            audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+            volumeSeekbar.setMax(audioManager
+                    .getStreamMaxVolume(AudioManager.STREAM_MUSIC));
+            volumeSeekbar.setProgress(audioManager
+                    .getStreamVolume(AudioManager.STREAM_MUSIC));
+
+
+            volumeSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
+            {
+                @Override
+                public void onStopTrackingTouch(SeekBar arg0)
+                {
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar arg0)
+                {
+                }
+
+                @Override
+                public void onProgressChanged(SeekBar arg0, int progress, boolean arg2)
+                {
+                    audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,
+                            progress, 0);
+                }
+            });
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
 }
