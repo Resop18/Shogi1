e406e3ba653e51443c93814018f4f0b9ab8b4ec4
Ryan <rtsfred3@gmail.com>
2017-03-09 13:45:59 -0800

    Add ShogiMoveAction

---
+++ app/src/main/java/edu/up/cs371/resop18/shogi/shogi/ShogiMoveAction.java
@@ -0,0 +1,31 @@
+package edu.up.cs371.resop18.shogi.shogi;
+
+import edu.up.cs371.resop18.shogi.game.GamePlayer;
+import edu.up.cs371.resop18.shogi.game.actionMsg.GameAction;
+
+/**
+ * @author Ryan Fredrickson
+ * @author Chase Des Laurier
+ */
+
+public class ShogiMoveAction extends GameAction {
+    private shogiPiece movedPiece = null;
+    private shogiPiece capturedPiece = null;
+
+    int row, col;
+
+    /**
+     * constructor for GameAction
+     *
+     * @param player the player who created the action
+     */
+    public ShogiMoveAction(GamePlayer player) {
+        super(player);
+    }
+
+    public void setMovedPiece(shogiPiece piece){ this.movedPiece = piece; }
+
+    public void movePiece(int newRow, int newCol){
+
+    }
+}
