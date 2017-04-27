4f8efeee825a5f5d47038218faa71870af41cf14
Ryan <rtsfred3@gmail.com>
2017-04-27 16:04:13 -0700

    Increased time delay for AI move

--- app/src/main/java/edu/up/cs371/resop18/shogi/shogi/ShogiHardComputerPlayer.java
+++ app/src/main/java/edu/up/cs371/resop18/shogi/shogi/ShogiHardComputerPlayer.java
@@ -20,7 +20,7 @@ public class ShogiHardComputerPlayer extends GameComputerPlayer {
     protected void receiveInfo(GameInfo info) {
         if(info instanceof ShogiGameState){
             this.state = (ShogiGameState)info;
-            sleep(1000); //Sleeps for 1 second before making move
+            sleep(2500); //Sleeps for 2.5 second before making move
 
             //Smart AI, which will check if an enemy piece can be captured, otherwhite randomly makes moves
             ShogiDumbAI ai = new ShogiDumbAI(state, game);
