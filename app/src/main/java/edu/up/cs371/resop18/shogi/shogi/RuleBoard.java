c71cc0dc67eedd592339a1ef6d6d63c3212874d1
nguyenb19 <nguyenb19@up.edu>
2017-04-27 15:51:55 -0700

    Option theme + theme + update menu 90%

---
+++ app/src/main/java/edu/up/cs371/resop18/shogi/shogi/RuleBoard.java
@@ -0,0 +1,40 @@
+
+package edu.up.cs371.resop18.shogi.shogi;
+
+        import android.content.Context;
+        import android.graphics.Bitmap;
+        import android.graphics.BitmapFactory;
+        import android.graphics.Canvas;
+        import android.graphics.Color;
+        import android.util.AttributeSet;
+        import android.view.SurfaceView;
+
+        import edu.up.cs371.resop18.shogi.R;
+
+/**
+ * Created by Admin on 4/21/2017.
+ */
+
+public class RuleBoard extends SurfaceView {
+
+    public RuleBoard(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setWillNotDraw(false);
+    }
+
+    @Override
+    public void onDraw(Canvas canvas)
+    {
+        //canvas.drawColor(Color.WHITE);
+
+
+        Bitmap myImageBMP2 = BitmapFactory.decodeResource(getResources(), R.drawable.rude);
+        //canvas.drawColor(Color.WHITE);
+        canvas.drawBitmap(myImageBMP2, 10, 10, null);
+
+
+
+    }
+
+}
+
