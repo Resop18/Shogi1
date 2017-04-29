aeea2f577a79d1aceda427909374f39fab9b8488
Ryan <rtsfred3@gmail.com>
2017-04-29 16:05:07 -0700

    Time delay for AI is now 1 second

--- app/src/main/java/edu/up/cs371/resop18/shogi/shogi/ShogiDumbAI.java
+++ app/src/main/java/edu/up/cs371/resop18/shogi/shogi/ShogiDumbAI.java
@@ -18,7 +18,7 @@ public class ShogiDumbAI {
     public ShogiDumbAI(ShogiGameState currState, Game currGame){
         this.game = currGame;
         this.state = currState;
-        sleep(500); //Sleeps for 2 second before making move
+        sleep(1000); //Sleeps for 1 second before making move
     }
 
     public void sleep(int time){
