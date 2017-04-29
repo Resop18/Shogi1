ed51184e643acdaec3afe1927619fcb1650b4928
Ryan <rtsfred3@gmail.com>
2017-04-28 21:28:24 -0700

    Removed unused variables in ShogiDumbComputerPlayer

--- app/src/main/java/edu/up/cs371/resop18/shogi/shogi/ShogiDumbComputerPlayer.java
+++ app/src/main/java/edu/up/cs371/resop18/shogi/shogi/ShogiDumbComputerPlayer.java
@@ -11,14 +11,6 @@ import edu.up.cs371.resop18.shogi.game.infoMsg.GameInfo;
 
 public class ShogiDumbComputerPlayer extends GameComputerPlayer {
     private ShogiGameState state; //Declaration of ShogiGameState
-    private ShogiPiece[][] board; //Declaration of the board
-    private LegalMoves getLegalMoves = new LegalMoves(1); //Sets LegalMoves for dumb AI
-    private int row, col, newRow, newCol; //Declares the old row, old col, new row, and new col
-    private ShogiPiece piece; //Declares the piece moved
-
-    private boolean pieceSelection = false;
-
-
 
     public ShogiDumbComputerPlayer(String name) { super(name); }
 
